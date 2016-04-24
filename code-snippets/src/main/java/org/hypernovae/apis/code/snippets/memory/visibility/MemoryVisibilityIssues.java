5d7aa21bf0c10b9fd68cb4c3c3adc1e93e4bf66f
alainlompo <alain.lompo@gmail.com>
2016-04-24 21:56:33 +0100

    A code snippet about memory visibility issues

---
+++ code-snippets/src/main/java/org/hypernovae/apis/code/snippets/memory/visibility/MemoryVisibilityIssues.java
@@ -0,0 +1,35 @@
+package org.hypernovae.apis.code.snippets.memory.visibility;
+
+public class MemoryVisibilityIssues {
+	private static boolean isReady = false;
+	private static Integer intObjValue;
+	
+	private static class ReadingThread extends Thread {
+		public void run() {
+			while(!isReady) {
+				// Gives a hint to the thread scheduler
+				// That the current thread is willing to yield its current
+				// use of the processor. If it is not ignored by the scheduler
+				// It will result in a positive impact on performances (it's a heuristic)
+				Thread.yield();
+			}
+			
+			System.out.println(intObjValue);
+		}
+		
+		/**
+		 * This practice is to be avoided.
+		 * It might seem obvious that this program will print 120, and it will do so often, but there
+		 * is a possibility that it never ends or it prints null (non initialized intObjValue)
+		 * @param args
+		 */
+		public static void main(String[] args) {
+			new ReadingThread().start();
+			intObjValue = 120;
+			isReady = true;
+		}
+		
+		
+	}
+
+}
