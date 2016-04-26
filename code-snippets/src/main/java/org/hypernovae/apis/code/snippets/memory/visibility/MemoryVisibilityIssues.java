2b1ca7a052ca2510a903981a6e33b69710214a90
alainlompo <alain.lompo@gmail.com>
2016-04-26 03:01:40 +0100

    Memory visibility issues ==> stale datas and synchronization

--- code-snippets/src/main/java/org/hypernovae/apis/code/snippets/memory/visibility/MemoryVisibilityIssues.java
+++ code-snippets/src/main/java/org/hypernovae/apis/code/snippets/memory/visibility/MemoryVisibilityIssues.java
@@ -21,6 +21,15 @@ public class MemoryVisibilityIssues {
 		 * This practice is to be avoided.
 		 * It might seem obvious that this program will print 120, and it will do so often, but there
 		 * is a possibility that it never ends or it prints null (non initialized intObjValue)
+		 * As a matter of fact insufficient synchronization can produce stale datas. the ReadingThread instance
+		 * might see an out - of - date isReady value.
+		 * Stale datas can be the source of important issues such as:
+		 * ==> infinite loops
+		 * ==> inaccurate computations
+		 * ==> unexpected exceptions
+		 * ==> corrupted data structures
+		 * When the access to a given data is synchronized there is a guaranty that two threads accessing
+		 * successfully to the data using the same will see the up - to - date value of the data
 		 * @param args
 		 */
 		public static void main(String[] args) {
